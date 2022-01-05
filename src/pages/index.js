90c3b1f2f66cefbc134e0d5f683a867cd3237e6a
celectrico <krowewbyte@gmail.com>
2022-01-05 20:35:32 -0300

    full logic para agregar productos y ordenarlos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from 'gatsby'
+import MainContent from "../components/MainContent";
 import Section from "../components/Section";
 import Item from "../components/Item";
-import MainContent from "../components/MainContent";
 
 // markup
 const IndexPage = ({data}) => {
@@ -12,23 +12,43 @@ const IndexPage = ({data}) => {
 
   const productos = [];
 
+  console.log(edges)
+  console.log(productos)
   edges.map(edge => (
     productos.push(edge.node)
   ))
 
-  console.log(edges)
-  console.log(productos)
-  
+  // Accepts the array and key
+const groupBy = (array, key) => {
+  // Return the end result
+  return array.reduce((result, currentValue) => {
+    // If an array already present for key, push it to the array. Else create an array and push the object
+    (result[currentValue[key]] = result[currentValue[key]] || []).push(
+      currentValue
+    );
+    // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
+    return result;
+  }, []); // empty object is the initial value for result object
+};
+
+// Group by color as key to the person array
+const categorias = groupBy(productos, 'categoria');
+
+console.log(categorias)
 
   return (
 
     <Layout>
       <MainContent>
-        {productos.map(producto => (
-          <Section categoria={producto.categoria}>
-            <Item nombre={producto.item} traduccion={producto.traduccion} precio={producto.precio} />
-          </Section>
-        ))}
+        {Object.keys(categorias).map((key) => {
+          return (
+            <Section categoria={key}>
+              {categorias[key].map(item => (
+                <Item nombre={item.item} traduccion={item.traduccion} precio={item.precio} />
+              ))}
+            </Section>
+          )
+        })}
       </MainContent>
     </Layout>
   )

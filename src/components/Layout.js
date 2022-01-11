ad68bc53baafc6efd0cdbd58f70fd2f4ae0e4079
celectrico <krowewbyte@gmail.com>
2022-01-11 17:44:10 -0300

    nuevo intento overscroll

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,7 +16,7 @@ const Layout = ({children}) => {
     `)
 
     return (
-        <div className="h-screen flex bg-white relative bg-black overscroll-auto">
+        <div className="h-screen flex bg-white relative bg-black">
             <title>{data.site.siteMetadata.title}</title>
             {children}
         </div>

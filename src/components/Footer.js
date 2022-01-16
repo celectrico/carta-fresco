012be064ba635215fd366d13e3c06000a342b7e1
celectrico <krowewbyte@gmail.com>
2022-01-15 21:52:35 -0300

    arreglo footer background

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="absolute bottom-0 left-0 text-center w-11/12 h-24 bg-white mt-2 pr-1 pl-1 pt-2 rounded-md shadow-md" >
+        <div className="absolute bottom-0 left-0 text-center w-full h-24 bg-white mt-2 pr-1 pl-1 pt-2" >
             <div className="flex justify-center w-full p-1">
                 <span>FRESCO BAR © {new Date().getFullYear()}</span>
             </div>
